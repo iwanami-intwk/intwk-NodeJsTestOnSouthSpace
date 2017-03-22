b42b371b57b4e8a68b704cf22eca54629cac6e56
iwanami-intwk <iwanami@intwk.co.jp>
2017-03-22 18:36:18 +0900

    Update example.js

--- example.js
+++ example.js
@@ -1,8 +1,30 @@
-const http = require('http');
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+var express = require("express");
+var app = express();
 
-http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(80);
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
 
-console.log('Server running at ひゃっはー');
+/* 3. 以後、アプリケーション固有の処理 */
+
+// 写真のサンプルデータ
+var photoList = [
+    {
+        id: "001",
+        name: "photo001.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo001.jpg"
+    },{
+        id: "002",
+        name: "photo002.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo002.jpg"
+    }
+]
+
+// 写真リストを取得するAPI
+app.get("/api/photo/list", function(req, res, next){
+    res.json(photoList);
+});
