a402c860bdd15513b38e410a3e18d1d113aab149
iwanami-intwk <iwanami@intwk.co.jp>
2017-03-22 17:55:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-/*
-// このアプリはWEBサーバーに express を利用します。
-// くわしくはこちら: http://expressjs.com
-var express = require('express');
-
-// cfenv は Cloud Foundry 環境へのアクセスを提供します。
-// くわしくはこちら: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// 新しい express サーバーを作成します。
-var app = express();
-
-// ./public からメインファイルを提供するようにします。
-app.use(express.static(__dirname + '/public'));
-
-//  Cloud Foundry からアプリ環境を取得します。
-var appEnv = cfenv.getAppEnv();
-
-// 指定されたポートとホストでアプリを起動。start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
-*/
-
-
-
-
 // Example 1: sets up service wrapper, sends initial message, and
 // receives response.
 
@@ -39,9 +5,9 @@ var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
 // Set up Conversation service wrapper.
 var conversation = new ConversationV1({
-  username: 'd860e3d0-d82a-4ca5-865b-aae26944f601', // replace with username from service key
-  password: '0ioEDkcFj1oS', // replace with password from service key
-  path: { workspace_id: 'Conversation-firstService' }, // replace with workspace ID
+  username: 'USERNAME', // replace with username from service key
+  password: 'PASSWORD', // replace with password from service key
+  path: { workspace_id: 'WORKSPACE_ID' }, // replace with workspace ID
   version_date: '2016-07-11'
 });
 
