3ab8c02fed6095edf853056126621155dc2f3187
iwanami-intwk <iwanami@intwk.co.jp>
2017-03-22 17:28:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 var conversation = new ConversationV1({
   username: 'd860e3d0-d82a-4ca5-865b-aae26944f601', // replace with username from service key
   password: '0ioEDkcFj1oS', // replace with password from service key
-  path: { workspace_id: 'WORKSPACE_ID' }, // replace with workspace ID
+  path: { workspace_id: 'Conversation-firstService' }, // replace with workspace ID
   version_date: '2016-07-11'
 });
 
