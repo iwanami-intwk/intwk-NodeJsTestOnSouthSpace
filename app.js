a46ae3929a259288d4c68dc66a2cf167a133ebc0
iwanami-intwk <iwanami@intwk.co.jp>
2017-03-22 18:24:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,20 @@
 // このアプリはWEBサーバーに express を利用します。
 // くわしくはこちら: http://expressjs.com
 var express = require('express');
+
 // cfenv は Cloud Foundry 環境へのアクセスを提供します。
 // くわしくはこちら: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
+
 // 新しい express サーバーを作成します。
 var app = express();
+
 // ./public からメインファイルを提供するようにします。
 app.use(express.static(__dirname + '/public'));
+
 //  Cloud Foundry からアプリ環境を取得します。
 var appEnv = cfenv.getAppEnv();
+
 // 指定されたポートとホストでアプリを起動。start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
